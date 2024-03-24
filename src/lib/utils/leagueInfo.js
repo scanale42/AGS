41f4a4896c33b575b9a9ce6a197f6367cab64676
scanale42 <scanale42@gmail.com>
2024-03-23 22:06:08 -0400

    Update leagueInfo.js

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -8,6 +8,8 @@ export const enableBlog = false; // requires VITE_CONTENTFUL_ACCESS_TOKEN and VI
 /*   STEP 2   */
 export const homepageText = `
   <p>Any Given Sunday: A fantasy football league created by degenerates for degenerates.</p>
+  <p>Test</p>
+  
 
 `;
 
