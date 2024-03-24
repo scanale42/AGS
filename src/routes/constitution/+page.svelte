a19af7289c5e00fcdc08341bd8bea5d3ab4d1279
scanale42 <scanale42@gmail.com>
2024-03-24 00:09:20 -0400

    Update +page.svelte

--- src/routes/constitution/+page.svelte
+++ src/routes/constitution/+page.svelte
@@ -258,7 +258,7 @@
     <h2 class="sectionHeading" bind:this={seven}>Section 7 League Finances</h2>
     
     <h3 bind:this={sevenOne}>7.1 League Dues</h3>
-    <p>League dues are set at $50. Dues are collected through <a href="https://www.leaguesafe.com/league/3949641">LeagueSafe</a> and collection will be sent out no later than the first of August. All managers must be fully paid up before the start of regular season. In the event that a manager fails to pay before the deadline, a 10% penalty will be addd to their payment and will go towards the payouts at the end of the year.</p>
+    <p>League dues are set at $50. Dues are collected through <a href="https://www.leaguesafe.com/league/3949641">LeagueSafe</a> and dues must be collected no later than 2 weeks before the posted start of the next season's rooke draft. </p>
     
     <h3 bind:this={sevenTwo}>7.2 Payout</h3>
     <p>League payout is structured as follows:</p>
