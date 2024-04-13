8c7fabf69ec41e51e353c7cabeeefd52cd71d651
scanale42 <scanale42@gmail.com>
2024-04-12 22:01:02 -0400

    Update leagueInfo.js

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -94,7 +94,7 @@ export const managers = [
       "name": "dwr02000",
       "location": "Location: ???", // (optional)
       "bio": "Biography Incoming ",
-      "photo": "/managers/no_picture.jpg", // square ratio recommended (no larger than 500x500)
+      "photo": "/managers/sanfran.png", // square ratio recommended (no larger than 500x500)
       "fantasyStart": 9229, // (optional) when did the manager start playing fantasy football
       "favoriteTeam": "sf", // (optional) favorite NFL team, (follows convention: nyj, sea, mia, etc.) MUST BE LOWERCASE
       "mode": "Win Now", // (optional) 'Win Now', 'Dynasty', or 'Rebuild' (anything else and you will need to add a new png to /static/ similar to the 'Rebuild.png' and 'Win Now.png' currently in there)
@@ -116,7 +116,7 @@ export const managers = [
       "name": "Jamesobs51",
       "location": "Location: ???", // (optional)
       "bio": "Biography Incoming ",
-      "photo": "/managers/no_picture.jpg", // square ratio recommended (no larger than 500x500)
+      "photo": "/managers/chicago.png", // square ratio recommended (no larger than 500x500)
       "fantasyStart": 2024, // (optional) when did the manager start playing fantasy football
       "favoriteTeam": "chi", // (optional) favorite NFL team, (follows convention: nyj, sea, mia, etc.) MUST BE LOWERCASE
       "mode": "Win Now", // (optional) 'Win Now', 'Dynasty', or 'Rebuild' (anything else and you will need to add a new png to /static/ similar to the 'Rebuild.png' and 'Win Now.png' currently in there)
@@ -138,7 +138,7 @@ export const managers = [
       "name": "Mademan",
       "location": "Location: ???", // (optional)
       "bio": "Biography Incoming ",
-      "photo": "/managers/no_picture.jpg", // square ratio recommended (no larger than 500x500)
+      "photo": "/managers/chiefs.png", // square ratio recommended (no larger than 500x500)
       "fantasyStart": 2024, // (optional) when did the manager start playing fantasy football
       "favoriteTeam": "kc", // (optional) favorite NFL team, (follows convention: nyj, sea, mia, etc.) MUST BE LOWERCASE
       "mode": "Win Now", // (optional) 'Win Now', 'Dynasty', or 'Rebuild' (anything else and you will need to add a new png to /static/ similar to the 'Rebuild.png' and 'Win Now.png' currently in there)
@@ -182,7 +182,7 @@ export const managers = [
       "name": "molson2111",
       "location": "Location: ???", // (optional)
       "bio": "Biography Incoming ",
-      "photo": "/managers/no_picture.jpg", // square ratio recommended (no larger than 500x500)
+      "photo": "/managers/molson.png", // square ratio recommended (no larger than 500x500)
       "fantasyStart": 2024, // (optional) when did the manager start playing fantasy football
       "favoriteTeam": "sf", // (optional) favorite NFL team, (follows convention: nyj, sea, mia, etc.) MUST BE LOWERCASE
       "mode": "Win Now", // (optional) 'Win Now', 'Dynasty', or 'Rebuild' (anything else and you will need to add a new png to /static/ similar to the 'Rebuild.png' and 'Win Now.png' currently in there)
@@ -226,7 +226,7 @@ export const managers = [
       "name": "BoyzIIBen",
       "location": "Location: ???", // (optional)
       "bio": "Biography Incoming ",
-      "photo": "/managers/bozy2ben.png", // square ratio recommended (no larger than 500x500)
+      "photo": "/managers/boyz2ben.png", // square ratio recommended (no larger than 500x500)
       "fantasyStart": 2024, // (optional) when did the manager start playing fantasy football
       "favoriteTeam": "lac", // (optional) favorite NFL team, (follows convention: nyj, sea, mia, etc.) MUST BE LOWERCASE
       "mode": "Win Now", // (optional) 'Win Now', 'Dynasty', or 'Rebuild' (anything else and you will need to add a new png to /static/ similar to the 'Rebuild.png' and 'Win Now.png' currently in there)
